118bf3023b06a05f6c3923f7e45cf9b72eb548c0
Ronnie-88 <50335505+Ronnie-88@users.noreply.github.com>
2023-06-17 16:55:13 -0700

    Three hit combo (#16)
    Added a 3 hit combo that requires some timing to execute in sequence.

--- Assets/MT_Scripts/Player.cs
+++ Assets/MT_Scripts/Player.cs
@@ -8,18 +8,34 @@ public class Player : MonoBehaviour
     private float mMoveSpeed = 6.0f;
     [SerializeField]
     private float mJumpHeight = 2.0f;
+    [SerializeField]
+    private float mLastComboEndDelay = 0.5f;
+    [SerializeField]
+    private float mLastHitDelay = 0.2f;
     private MT_InputAsset mPlayerInput;
     private Action<InputAction.CallbackContext> mMoveAction;
     private Action<InputAction.CallbackContext> mCancelMoveAction;
     private Action<InputAction.CallbackContext> mJumpAction;
+    private Action<InputAction.CallbackContext> mAttackAction;
     private float mMoveInputVector;
     private Vector2 mMoveVector;
     private Rigidbody2D mMyRb;
+    private Animator mAnimator;
+    private SpriteRenderer mPlayerSprite;
+    private float mPlayerAnimSpeed;
+    private bool flipPlayer;
+    private int mComboLength = 3;
+    private int mComboCounter = 0;
+    private float mLastComboEndTime;
+    private float mLastHitTime;
+    private bool mCanInvokeEndCombo;
 
     private void Awake()
     {
         mPlayerInput = new MT_InputAsset();
         mMyRb = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponentInChildren<Animator>();
+        mPlayerSprite = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void OnEnable()
@@ -27,6 +43,7 @@ public class Player : MonoBehaviour
         mMoveAction = (context) =>
         {
             mMoveInputVector = context.ReadValue<float>();
+            flipPlayer = mMoveInputVector < 0.0f ? true : false;
         };
         mCancelMoveAction = (context) =>
         {
@@ -36,23 +53,36 @@ public class Player : MonoBehaviour
         {
             Jump();
         };
+        mAttackAction = (context) =>
+        {
+            Attack();
+        };
         mPlayerInput.GamePlay.Run.performed += mMoveAction;
         mPlayerInput.GamePlay.Run.canceled += mCancelMoveAction;
         mPlayerInput.GamePlay.Jump.performed += mJumpAction;
+        mPlayerInput.GamePlay.StandardAttack.performed += mAttackAction;
         mPlayerInput.GamePlay.Enable();
     }
+
 
     private void OnDisable()
     {
         mPlayerInput.GamePlay.Run.performed -= mMoveAction;
         mPlayerInput.GamePlay.Run.canceled -= mCancelMoveAction;
         mPlayerInput.GamePlay.Jump.performed -= mJumpAction;
+        mPlayerInput.GamePlay.StandardAttack.performed -= mAttackAction;
         mPlayerInput.GamePlay.Disable();
+    }
+
+    private void Update()
+    {
+        mAnimator.SetBool("IsMoving", mMyRb.velocity.x != 0.0f);
+        mPlayerSprite.flipX = flipPlayer;
+        TryToExitAttack();
     }
 
     private void FixedUpdate()
     {
-        // mMyRb.velocity.x < mMoveSpeed || mMyRb.velocity.x  > -mMoveSpeed
         _handleHorizontalMovement();
     }
 
@@ -87,6 +117,37 @@ public class Player : MonoBehaviour
     {
         _cancelVerticalVelocity();
         mMyRb.AddForce(transform.up * mJumpHeight * mMyRb.mass, ForceMode2D.Impulse);
+    }
+
+    private void Attack()
+    {
+        //check if can combo
+        if (Time.time - mLastComboEndTime > mLastComboEndDelay && mComboCounter < mComboLength)
+        {
+            if (Time.time - mLastHitTime >= mLastHitDelay)
+            {
+                mAnimator.SetTrigger("Attack"+mComboCounter);
+                //other Attack properties are set here
+                mComboCounter++;
+                mLastHitTime = Time.time;
+                mCanInvokeEndCombo = true;
+            }
+        }
+    }
+
+    private void TryToExitAttack()
+    {
+        if (mAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.9f && mAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack") && mCanInvokeEndCombo)
+        {
+            EndCombo();
+            mCanInvokeEndCombo = false;
+        }
+    }
+
+    private void EndCombo()
+    {
+        mComboCounter = 0;
+        mLastComboEndTime = Time.time;
     }
 
 }
